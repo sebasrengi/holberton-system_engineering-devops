3a03484fdb779e31ee60297d84fbfb1a66067d85
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-07 15:09:01 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,3 @@
 # fixes Apache 500 error by fixing typo in wordpress
 # Fiz error 500 Internal server error
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/'
-    }
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
