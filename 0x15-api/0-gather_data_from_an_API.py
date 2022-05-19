3781e27a5aa40f9b1cee3cfbf7147c1e8de14f93
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-19 15:12:31 -0500

    fail

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,1 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
-
-if __name__ == "__main__":
-    import urllib.request as request
-
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        readcontent = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(readcontent)))
-        print("\t- content: {}".format(readcontent))
-        print("\t- utf8 content: {}".format(readcontent.decode('utf8')))
