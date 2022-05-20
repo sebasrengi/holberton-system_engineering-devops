70bb6afe06cdf49d99c3bf7368c261d1eb5f15c8
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-19 20:18:45 -0500

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,28 @@
-import requests
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
 from sys import argv
+import requests
 
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(argv[1])
+    users = requests.get(url + "/users/{}".
+                        format(argv[1]), verify=False).json()
+    todos = requests.get(url + "/todos?userId={}".
+                        format(argv[1]), verify=False).json()
+    
+    all_user = requests.get(
+        url + /todos?userId={}".
+        format(user_id)).json()
+    lists = []
 
-if __name__ == "__main__":
-    if len(argv) > 1:
-        userId = int(argv[1])
-        url = "https://jsonplaceholder.typicode.com/users/"
-        r = requests.get("{}/{}".format(url, userId))
-        data = r.json()
-        name = r.json().get("name")
-        if name is not None:
-            userTodos = requests.get("{}{}/todos".format(url, userId))
-            userTodos = userTodos.json()
-            taskNumber = len(userTodos)
-            completedTasks = []
-            for task in userTodos:
-                if task.get("completed") is True:
-                    completedTasks.append(task)
-            taskCompletedNumber = len(completedTasks)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, taskCompletedNumber, taskNumber))
-            for task in completedTasks:
-                title = task.get("title")
-                print("\t {}".format(title))
+    for task in todos:
+         if task.get('completed') is True:
+            lists.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(lists), len(all_user)))
+    print("\n".join("\t {}".format(task) for task in lists))
