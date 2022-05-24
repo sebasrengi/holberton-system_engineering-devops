b61a5b885560bc520112878ef3cb2f5d75ebc139
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-23 21:58:49 -0500

    Update 0-main.py

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 0-main
 """
@@ -5,7 +6,4 @@ import sys
 
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+    print("{:d}".format(number_of_subscribers("programming")))
